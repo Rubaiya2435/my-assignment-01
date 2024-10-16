71c3326cc91577dacbc67f3b45a4f59f4d2b5696
Rubaiya2435 <164077424+Rubaiya2435@users.noreply.github.com>
2024-10-16 23:01:24 +0500

    my-assignment-01
    This is my first project of next.js

---
+++ app/services/page.tsx
@@ -0,0 +1,11 @@
+
+
+import React from 'react'
+
+export default function ServicesPage() {
+  return (
+    <div className='bg-blue-700 text-white'>
+      <h1>This is my Services Page</h1>
+    </div>
+  )
+}
